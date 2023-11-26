a147bfd496bccdfd3f1fa39fd230db002b14db90
Robin Dacccahe <robindaccache@outlook.com>
2023-11-26 15:35:52 +0200

    fixed structure of the project and added new pages and styles

--- scripts/portfolio-app.js
+++
@@ -1,9 +0,0 @@
-const portfolio1 = document.querySelector('.portfolio');
-const portfolio2 = document.querySelector('.portfolio');
-
-function open_portfolio_1() {  
-    portfolio1.style.display="block";
-}
-
-function open_portfolio_2(){
-}
