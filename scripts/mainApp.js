4ae8ca3700558467abc27d76d39b12f6a212f2c9
Robin Dacccahe <robindaccache@outlook.com>
2023-11-27 19:32:34 +0200

    fixed sidebar issues

--- scripts/mainApp.js
+++ scripts/mainApp.js
@@ -27,16 +27,35 @@ menuButton.addEventListener('click', () => {
 });
 
 function toggle_sidebar(){
+
+    
     if(sidebar.classList.contains("sidebar-close")){
         sidebar.classList.add("sidebar-open");
         sidebar.classList.remove('sidebar-close');   
         arrowRight.classList.add('fa-chevron-right-closing');
-        closeButton.style.transform = 'translateX(12rem)'
+        closeButton.style.transform = 'translateX(12rem)';
+        document.body.style.marginRight = '10px';
     }
     else{
         sidebar.classList.remove('sidebar-open');
         sidebar.classList.add('sidebar-close');
         arrowRight.classList.remove('fa-chevron-right-closing');
-        closeButton.style.transform = 'translateX(0rem)'
+        closeButton.style.transform = 'translateX(0rem)';
+        document.body.style.marginRight = '0px';
     }
+
+    const overlay = document.getElementById('overlay');
+
+    overlay.style.display = (overlay.style.display === 'none' || overlay.style.display === '') ? 'block' : 'none';
+    document.body.style.overflow = (overlay.style.display === 'none' || overlay.style.display === '') ? 'auto' : 'hidden';
 }
+
+
+document.addEventListener('click', function (event) {
+
+    if (!sidebar.contains(event.target) && event.target !== closeButton) {
+        if (sidebar.classList.contains("sidebar-open")) {
+            toggle_sidebar();
+        }
+    }
+});
